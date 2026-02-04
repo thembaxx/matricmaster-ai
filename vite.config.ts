f0f31b6c2905798d5b05f64b42ce0076bb0676f7
Themba <mndebele.themba@gmail.com>
2026-02-04 11:08:55 +0200

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
-	
+
 	return {
 		server: {
 			port: 3000,
 			host: '0.0.0.0',
 		},
-		plugins: [react(), tailwindcss()],
+		plugins: [react()],
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src'),
